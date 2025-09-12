e5c598383ef1648169e0bc98ee0831d5840c416c
Ramya Patiwada <ramya.patiwada@example.com>
2025-09-12 11:32:22 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+
+// const heading=React.createElement("h1",{},"HELLO WORLD USING REACT");
+
+/* <div id="parent">
+    <div id="child">
+        <h1>1st heading</h1>
+        <h2>sibling</h2>
+    </div>
+</div> */
+
+
+const parent=React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},[React.createElement("h1",{},"1st heading"),
+    React.createElement("h1",{},"i am a sibling") 
+    ]));
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
