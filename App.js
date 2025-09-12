be77c15cf71f181cc7e8a7767ce7ae3884a2e603
RamyaPativada <ramyapativada@gmail.com>
2025-09-12 15:20:10 +0530

    using npm

--- App.js
+++ App.js
@@ -6,7 +6,8 @@
         <h2>sibling</h2>
     </div>
 </div> */
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent=React.createElement("div",{id:"parent"},
     React.createElement("div",{id:"child"},[React.createElement("h1",{},"1st heading"),
